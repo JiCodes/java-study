b0a32ddca35e87d2c466922a16fb31786e8d8308
JiCodes <liu.ji1@northeastern.edu>
2023-04-14 22:46:32 -0700

    ElementExchange class with exchange method

---
+++ exercises/exercise07/src/ElementExchange.java
@@ -0,0 +1,8 @@
+public class ElementExchange {
+    public static <T> void exchange(T[] array, int index1, int index2) {
+        T temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+}
--- exercises/exercise07/src/Main.java
+++ exercises/exercise07/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 public class Main {
@@ -11,10 +12,18 @@ public class Main {
         for (int i = 2; i < 25; i++) {
             numbers.add(i);
         }
-
         // Call the countPrimes method to count the number of prime numbers in the collection
         int numPrimes = PrimeCounter.countPrimes(numbers);
         // Print the number of prime numbers in the collection
         System.out.println("Number of primes in the collection: " + numPrimes);
+
+        // Test Element Exchange
+        Integer[] items = {1, 2, 3, 4, 5};
+        System.out.println("Origin Array: " + Arrays.toString(items));
+
+        ElementExchange.exchange(items, 1, 3);
+        System.out.println("Array after exchange: " + Arrays.toString(items));
+
+
     }
 }
